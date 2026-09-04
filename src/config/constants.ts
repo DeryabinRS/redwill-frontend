37c2166e60811bb618763cad9cfe04516a18fa9e
Deryabin Roman <46558697+DeryabinRS@users.noreply.github.com>
2026-09-04 13:09:13 +0700

    update typescript

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
+//*Данные для загрузки мото https://vehiclesdb.com/motorcycles*/
 export const SITE_NAME = import.meta.env.VITE_APP_SITE_NAME;
 export const API_URL = import.meta.env.VITE_APP_API_URL;
 export const GOOGLE_RECAPTCHA_SITE_KEY = import.meta.env.VITE_APP_GOOGLE_RECAPTCHA_SITE_KEY;
